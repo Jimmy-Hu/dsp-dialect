f4cd6d326d4cabfc4f8efc5dabeb9cb69dcdf284
jimmyhu <5549662+Jimmy-Hu@users.noreply.github.com>
2026-07-04 06:01:42 +0800

    Update lit.cfg.py
    Update lit.cfg.py

--- test/lit.cfg.py
+++ test/lit.cfg.py
@@ -1,23 +1,17 @@
 # -*- Python -*-
 
 import os
-import platform
-import re
-import subprocess
-import tempfile
-
 import lit.formats
 import lit.util
 
 from lit.llvm import llvm_config
-from lit.llvm.subst import ToolSubst
-from lit.llvm.subst import FindTool
 
 # Configuration file for the 'lit' test runner.
 
 # name: The name of this test suite.
-config.name = 'STANDALONE'
+config.name = 'DSP'
 
+# testFormat: The test format to use to interpret tests.
 config.test_format = lit.formats.ShTest(not llvm_config.use_lit_shell)
 
 # suffixes: A list of file extensions to treat as test files.
@@ -26,31 +20,16 @@ config.suffixes = ['.mlir']
 # test_source_root: The root path where tests are located.
 config.test_source_root = os.path.dirname(__file__)
 
-# test_exec_root: The root path where tests should be run.
-config.test_exec_root = os.path.join(config.dsp_obj_root, 'test')
-
-config.substitutions.append(('%PATH%', config.environment['PATH']))
-
-llvm_config.with_system_environment(
-    ['HOME', 'INCLUDE', 'LIB', 'TMP', 'TEMP'])
-
+# Add system environment variables that might be needed by the compiler
+llvm_config.with_system_environment(['HOME', 'INCLUDE', 'LIB', 'TMP', 'TEMP'])
 llvm_config.use_default_substitutions()
 
-# excludes: A list of directories to exclude from the testsuite. The 'Inputs'
-# subdirectories contain auxiliary inputs for various tests in their parent
-# directories.
-config.excludes = ['Inputs', 'Examples', 'CMakeLists.txt', 'README.txt', 'LICENSE.txt']
+# excludes: A list of directories to exclude from the testsuite.
+config.excludes = ['CMakeLists.txt', 'README.txt', 'LICENSE.txt']
 
 # test_exec_root: The root path where tests should be run.
 config.test_exec_root = os.path.join(config.dsp_obj_root, 'test')
 config.dsp_tools_dir = os.path.join(config.dsp_obj_root, 'bin')
 
-# Tweak the PATH to include the tools dir.
-llvm_config.with_environment('PATH', config.llvm_tools_dir, append_path=True)
-
-tool_dirs = [config.dsp_tools_dir, config.llvm_tools_dir]
-tools = [
-    'dsp-opt'
-]
-
-llvm_config.add_tool_substitutions(tools, tool_dirs)
+# Add the DSP tools directory to the path so that 'dsp-opt' can be found during tests.
+llvm_config.with_environment('PATH', config.dsp_tools_dir, append_path=True)
