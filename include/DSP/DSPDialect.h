5b9d30d5d65524af58523c829c8cef30979eb76d
jimmyhu <5549662+Jimmy-Hu@users.noreply.github.com>
2026-07-06 07:37:44 +0800

    Update DSPDialect.h
    Update DSPDialect.h

--- include/DSP/DSPDialect.h
+++ include/DSP/DSPDialect.h
@@ -8,23 +8,4 @@
 // MLIR TableGen appends "Dialect.h.inc" suffix regardless of the .td file name.
 #include "DSP/DSPDialect.h.inc"
 
-namespace mlir
-{
-namespace dsp
-{
-    class DSPDialect : public ::mlir::Dialect
-    {
-    public:
-        explicit DSPDialect(::mlir::MLIRContext *context);
-        
-        static ::llvm::StringRef getDialectNamespace() 
-        {
-            return "dsp";
-        }
-        
-        void initialize();
-    };
-} // namespace dsp
-} // namespace mlir
-
 #endif // DSP_DSPDIALECT_H
